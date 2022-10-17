12e0bc0738891a293be3131028f1c741179d0b39
StarGrim33 <prosky33@gmail.com>
2022-10-17 18:43:32 +0300

    2
    Second commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int maxValue = 10;
+            int maxValue = 0;
             int value = 0;
             int position = 0;
             bool isProgrammOn = true;
@@ -13,17 +13,23 @@
             {
                 ShowMenu();
                 string userMenuNavigate = Console.ReadLine();
-                
-                switch(userMenuNavigate)
+                const string CommandHealthBar = "Healthbar";
+                const string CommandManaBar = "Manabar";
+                const string CommandExit = "Exit";
+
+                switch (userMenuNavigate)
                 {
-                    case "1":
+                    case CommandHealthBar:
                         DrawBar(value, maxValue, ConsoleColor.Red, position);
                         break;
-                    case "2":
+                    case CommandManaBar:
                         DrawBar(value, maxValue, ConsoleColor.Blue, position);
                         break;
-                    case "3":
+                    case CommandExit:
                         isProgrammOn = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неизвестная команда попробуйте еще раз");
                         break;
                 }
 
@@ -33,21 +39,27 @@
 
         static void DrawBar(int value, int maxValue, ConsoleColor color, int position)
         {
+            Console.WriteLine("Введите уровень здоровья/маны: ");
+            maxValue = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Введите какой % нужно закрасить: ");
             value = Convert.ToInt32(Console.ReadLine());
+
+            value = (value * maxValue) / 100;
+
             Console.WriteLine("Введите позицию: ");
             position = Convert.ToInt32(Console.ReadLine());
 
             Console.Clear();
             ConsoleColor defaultColor = Console.BackgroundColor;
             string bar = "";
-            
+
             for (int i = 0; i < value; i++)
             {
                 bar += ' ';
             }
 
-            Console.SetCursorPosition(0,position);
+            Console.SetCursorPosition(0, position);
             Console.Write('[');
             Console.BackgroundColor = color;
             Console.Write(bar);
@@ -55,18 +67,18 @@
 
             bar = "";
 
-            for(int i = value; i < maxValue; i++)
+            for (int i = value; i < maxValue; i++)
             {
                 bar += '#';
             }
 
             Console.Write(bar + ']');
         }
-        
+
         static void ShowMenu()
         {
-            Console.SetCursorPosition(10, 0);
-            Console.WriteLine("Здравствуйте! Какой элемент Вы хотите вывести на экран ? \n 1 - Healthbar \n 2 - Manabar \n 3 - Exit");
+            Console.SetCursorPosition(30, 0);
+            Console.WriteLine("Напишите какой элемент Вы хотите вывести на экран ? \n #-Healthbar \n #-Manabar \n #-Exit");
         }
 
     }
